ef54c1a324b8a1d90de1fe491460fdcc37e530e5
Garegin <garegin.makhmuryan@evollu.com>
2022-08-29 12:14:34 +0400

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,30 +18,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        User::create([
-            'name'=>'admin',
-            'email'=>'superadmin@mail.ru',
-            'password'=>'$2a$12$HEtD2CggESmYr5Zf5fdXe.am0CaS14YjEoMYVnFQT8QW7KLR5fRWe',
-            'mobile'=>'099999',
-            'reg_token'=>'Laravel',
-            'role'=>'admin'
-        ]);
-        User::create([
-            'name'=>'teacher1',
-            'email'=>'teacher1@mail.ru',
-            'password'=>'$2a$12$HEtD2CggESmYr5Zf5fdXe.am0CaS14YjEoMYVnFQT8QW7KLR5fRWe',
-            'mobile'=>'0111111',
-            'reg_token'=>'Laravel',
-            'role'=>'teacher'
-        ]);
-        User::create([
-            'name'=>'student1',
-            'email'=>'student1@mail.ru',
-            'password'=>'$2a$12$HEtD2CggESmYr5Zf5fdXe.am0CaS14YjEoMYVnFQT8QW7KLR5fRWe',
-            'mobile'=>'0111111',
-            'reg_token'=>'Laravel',
-            'role'=>'student'
-        ]);
+//        User::create([
+//            'name'=>'admin',
+//            'email'=>'superadmin@mail.ru',
+//            'password'=>'$2a$12$HEtD2CggESmYr5Zf5fdXe.am0CaS14YjEoMYVnFQT8QW7KLR5fRWe',
+//            'mobile'=>'099999',
+//            'reg_token'=>'Laravel',
+//            'role'=>'admin'
+//        ]);
+//        User::create([
+//            'name'=>'teacher1',
+//            'email'=>'teacher1@mail.ru',
+//            'password'=>'$2a$12$HEtD2CggESmYr5Zf5fdXe.am0CaS14YjEoMYVnFQT8QW7KLR5fRWe',
+//            'mobile'=>'0111111',
+//            'reg_token'=>'Laravel',
+//            'role'=>'teacher'
+//        ]);
+//        User::create([
+//            'name'=>'student1',
+//            'email'=>'student1@mail.ru',
+//            'password'=>'$2a$12$HEtD2CggESmYr5Zf5fdXe.am0CaS14YjEoMYVnFQT8QW7KLR5fRWe',
+//            'mobile'=>'0111111',
+//            'reg_token'=>'Laravel',
+//            'role'=>'student'
+//        ]);
         Course::create([
             'name'=>'Course 1'
         ]);
